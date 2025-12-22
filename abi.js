9b41b97e224301f537602e4112382d11d7057651
shahanetemadi <shahanetemadi7@gmail.com>
2025-12-22 05:07:43 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,25 +1,57 @@
 // abi.js
-export const ABI = [
+const ABI = [
+  {"inputs":[{"internalType":"contract IOrigin","name":"_origin","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},
+  {"inputs":[],"name":"AlreadyPurchased","type":"error"},
+  {"inputs":[],"name":"AlreadyRegistered","type":"error"},
+  {"inputs":[],"name":"AlreadyWithdrawn","type":"error"},
+  {"inputs":[],"name":"InvalidLength","type":"error"},
+  {"inputs":[],"name":"InvalidUsername","type":"error"},
+  {"inputs":[],"name":"MaximumLegs2","type":"error"},
+  {"inputs":[],"name":"MinimumWithdrawal","type":"error"},
+  {"inputs":[],"name":"ReentrancyGuardReentrantCall","type":"error"},
+  {"inputs":[],"name":"Ref1NotPurchased","type":"error"},
+  {"inputs":[],"name":"Ref2NotPurchased","type":"error"},
+  {"inputs":[],"name":"SelfReferralNotAllowed","type":"error"},
+  {"inputs":[],"name":"TakenUsername","type":"error"},
+  {"inputs":[],"name":"Terminated","type":"error"},
+
   {
-    "inputs": [
+    "inputs":[{"internalType":"string","name":"refer1","type":"string"},{"internalType":"string","name":"refer2","type":"string"}],
+    "name":"Purchase",
+    "outputs":[],
+    "stateMutability":"payable",
+    "type":"function"
+  },
+  {
+    "inputs":[{"internalType":"string","name":"username","type":"string"}],
+    "name":"Register",
+    "outputs":[],
+    "stateMutability":"payable",
+    "type":"function"
+  },
+  {
+    "inputs":[{"internalType":"address","name":"userAddr","type":"address"},{"internalType":"bool","name":"showNotifications","type":"bool"}],
+    "name":"UserBulkInfo",
+    "outputs":[
+      {"internalType":"string","name":"username","type":"string"},
       {
-        "internalType": "contract IOrigin",
-        "name": "_origin",
-        "type": "address"
+        "components":[
+          {"internalType":"uint256","name":"uniUsd","type":"uint256"},
+          {"internalType":"uint256","name":"biUsd","type":"uint256"},
+          {"internalType":"uint256","name":"vou","type":"uint256"},
+          {"internalType":"uint256","name":"mileUsd","type":"uint256"}
+        ],
+        "internalType":"struct BackBone.Balance",
+        "name":"latest",
+        "type":"tuple"
       }
     ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    "stateMutability":"view",
+    "type":"function"
   },
-  {
-    "inputs": [],
-    "name": "AlreadyPurchased",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "AlreadyRegistered",
-    "type": "error"
-  }
-  // ... ادامه ABI خودت بدون تغییر
+
+  {"inputs":[],"name":"UserCount","outputs":[{"internalType":"uint256","name":"count","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}
 ];
+
+export default ABI;
