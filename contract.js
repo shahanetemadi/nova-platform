0c7fc3943e4dcbb0dd6129046c18cf9bf1dac6a2
shahanetemadi <shahanetemadi7@gmail.com>
2025-12-22 04:41:15 +0330

    Refactor contract initialization and method calls

--- contract.js
+++ contract.js
@@ -2,23 +2,33 @@ import ABI from "./abi.js";
 import { CONTRACT_ADDRESS, RPC_URL } from "./config.js";
 
 let web3;
-let contract;
+let contractInstance;
 
 export async function initContract() {
   if (window.ethereum) {
     web3 = new Web3(window.ethereum);
   } else {
-    web3 = new Web3(new Web3.providers.HttpProvider(RPC_URL));
+    web3 = new Web3(
+      new Web3.providers.HttpProvider(RPC_URL)
+    );
   }
 
-  contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
-  return contract;
+  contractInstance = new web3.eth.Contract(
+    ABI,
+    CONTRACT_ADDRESS
+  );
+
+  return contractInstance;
+}
+
+export function getContract() {
+  return contractInstance;
 }
 
 export async function readOwner() {
-  return await contract.methods.owner().call();
+  return await contractInstance.methods.owner().call();
 }
 
 export async function readTotalSupply() {
-  return await contract.methods.totalSupply().call();
+  return await contractInstance.methods.totalSupply().call();
 }
