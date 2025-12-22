4f884807e625b30ef1252312d6cc395873f7763a
shahanetemadi <shahanetemadi7@gmail.com>
2025-12-22 05:04:58 +0330

    Remove unused error definitions from abi.js
    Removed multiple error definitions from the ABI.

--- abi.js
+++ abi.js
@@ -20,62 +20,6 @@ export const ABI = [
     "inputs": [],
     "name": "AlreadyRegistered",
     "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "AlreadyWithdrawn",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "InvalidLength",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "InvalidUsername",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "MaximumLegs2",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "MinimumWithdraw",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "ReentrancyGuard",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "Ref1NotPurchased",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "Ref2NotPurchased",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "SelfReferralNotAllowed",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "TakenUsername",
-    "type": "error"
-  },
-  {
-    "inputs": [],
-    "name": "Terminated",
-    "type": "error"
   }
-
-  // 👈 بقیه ABI رو همین‌طوری ادامه بده
+  // ... ادامه ABI خودت بدون تغییر
 ];
